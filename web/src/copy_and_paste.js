8c3866f2f81ab80a60c6d86e634bdaf294ee9494
Tim Abbott <tabbott@zulip.com>
2024-01-29 16:26:43 -0800

    copy_and_paste: Enable paste handler in production.

--- web/src/copy_and_paste.js
+++ web/src/copy_and_paste.js
@@ -4,7 +4,6 @@ import TurndownService from "turndown";
 
 import * as compose_ui from "./compose_ui";
 import * as message_lists from "./message_lists";
-import {page_params} from "./page_params";
 import * as rows from "./rows";
 
 function find_boundary_tr($initial_tr, iterate_row) {
@@ -584,8 +583,7 @@ export function paste_handler(event) {
         if (
             !compose_ui.cursor_inside_code_block($textarea) &&
             paste_html &&
-            !compose_ui.shift_pressed &&
-            page_params.development_environment
+            !compose_ui.shift_pressed
         ) {
             event.preventDefault();
             event.stopPropagation();
